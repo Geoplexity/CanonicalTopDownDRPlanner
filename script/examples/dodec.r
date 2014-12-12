98ada4a7e4be7988f283349ac6fd00f386708081
hongsly <hongsly@gmail.com>
2014-12-12 12:34:31 -0500

    Added several examples (with pictures showing the realization of subdivision)

---
+++ script/examples/dodec.r
@@ -0,0 +1,162 @@
+   Number_of_tile-types_(not_counting_the_four_standard_end_types):
+   3
+   Size_(i.e.,_number_of_edges)_of_each_of_these_types:
+   3 4 5
+   Subdivision-tiling:
+   Type_number_(the_first_one_should_be_4_since_the_ends_are_typed_0_1_2_3):
+   4
+   Number_of_tiles_into_which_that_tile_type_is_subdivided:
+   37
+   Type_of_each_of_these_tiles_in_the_subdivision:
+   4 6 5 6 5 6 5 4 5 4
+   5 4 5 4 5 4 5 6 5 6
+   5 6 5 6 5 4 5 4 5 6
+   5 4 5 4 5 5 6
+   Tile-ids_--_Adjacent_tiles_(in_correct_order_according_to_edge_number):
+   0 20 28 30
+   1 -1 -1 6 8 2
+   2 1 9 3 -1
+   3 -1 2 10 12 4
+   4 3 13 5 -1
+   5 -1 4 14 16 -1
+   6 1 -1 17 7
+   7 6 18 8
+   8 1 7 19 9
+   9 2 8 10
+   10 3 9 19 11
+   11 10 20 12
+   12 3 11 21 13
+   13 4 12 14
+   14 5 13 21 15
+   15 16 14 22
+   16 5 15 23 -1
+   17 -1 24 26 18 6
+   18 17 27 19 7
+   19 8 18 28 20 10
+   20 19 0 21 11
+   21 12 20 30 22 14
+   22 21 31 23 15
+   23 -1 16 22 32 34
+   24 17 -1 36 25
+   25 26 24 35
+   26 17 25 29 27
+   27 18 26 28
+   28 19 27 29 0
+   29 28 26 35 32 30
+   30 21 0 29 31
+   31 30 32 22
+   32 23 31 29 33
+   33 32 35 34
+   34 23 33 36 -1
+   35 29 25 36 33
+   36 -1 -1 34 35 24
+   Corresponding_boundary-tiling:
+   Boundary_of_type_number_(the_first_should_be_4):
+   4
+   Number_of_edges_before_subdivision:
+   3
+   Number_of_edges_into_which_each_of_these_original_edges_is_subdivided:
+   5 5 5
+   Original_edgeid_-_numbers_in_pairs_for_subdivision_edges_(tileno_edgeno):
+   0 1 0 2 3 3 0 4 3 5 0
+   1 36 0 24 1 17 0 6 1 1 1
+   2 5 4 16 3 23 0 34 3 36 1
+   Subdivision-tiling:
+   Type_number_(the_first_one_should_be_4_since_the_ends_are_typed_0_1_2_3):
+   5
+   Number_of_tiles_into_which_that_tile_type_is_subdivided:
+   31
+   Type_of_each_of_these_tiles_in_the_subdivision:
+   5 6 5 6 5 4 5 4 5 4
+   5 6 5 6 5 6 5 4 5 4
+   4 5 4 5 6 6 5 4 5 6
+   5
+   Tile-ids_--_Adjacent_tiles_(in_correct_order_according_to_edge_number):
+   0 13 19 24 20
+   1 -1 -1 4 6 2
+   2 1 7 3 -1
+   3 -1 2 8 10 -1
+   4 1 -1 11 5
+   5 4 12 6
+   6 1 5 13 7
+   7 2 6 8
+   8 3 7 13 9
+   9 10 8 14
+   10 3 9 15 -1
+   11 -1 16 18 12 4
+   12 11 19 13 5
+   13 6 12 0 14 8
+   14 13 20 15 9
+   15 -1 10 14 21 23
+   16 11 -1 25 17
+   17 18 16 26
+   18 11 17 24 19
+   19 12 18 0
+   20 14 0 21
+   21 15 20 24 22
+   22 21 28 23
+   23 15 22 29 -1
+   24 0 18 26 28 21
+   25 -1 -1 30 26 16
+   26 24 17 25 27
+   27 26 30 28
+   28 24 27 29 22
+   29 -1 -1 23 28 30
+   30 25 -1 29 27
+   Corresponding_boundary-tiling:
+   Boundary_of_type_number_(the_first_should_be_4):
+   5
+   Number_of_edges_before_subdivision:
+   4
+   Number_of_edges_into_which_each_of_these_original_edges_is_subdivided:
+   3 5 3 5
+   Original_edgeid_-_numbers_in_pairs_for_subdivision_edges_(tileno_edgeno):
+   0 1 0 2 3 3 0
+   1 25 0 16 1 11 0 4 1 1 1
+   2 29 0 30 1 25 1
+   3 3 4 10 3 15 0 23 3 29 1
+   Subdivision-tiling:
+   Type_number_(the_first_one_should_be_4_since_the_ends_are_typed_0_1_2_3):
+   6
+   Number_of_tiles_into_which_that_tile_type_is_subdivided:
+   21
+   Type_of_each_of_these_tiles_in_the_subdivision:
+   6 6 5 6 5 4 5 4 5 4
+   5 6 5 4 5 4 5 6 5 6
+   5
+   Tile-ids_--_Adjacent_tiles_(in_correct_order_according_to_edge_number):
+   0 8 6 12 14 16
+   1 -1 -1 4 6 2
+   2 1 7 3 -1
+   3 -1 2 8 10 -1
+   4 1 -1 11 5
+   5 4 12 6
+   6 1 5 0 7
+   7 2 6 8
+   8 3 7 0 9
+   9 10 8 16
+   10 3 9 17 -1
+   11 -1 -1 18 12 4
+   12 0 5 11 13
+   13 12 18 14
+   14 0 13 19 15
+   15 16 14 20
+   16 17 9 0 15
+   17 -1 -1 10 16 20
+   18 11 -1 19 13
+   19 -1 -1 20 14 18
+   20 17 15 19 -1
+   Corresponding_boundary-tiling:
+   Boundary_of_type_number_(the_first_should_be_4):
+   6
+   Number_of_edges_before_subdivision:
+   5
+   Number_of_edges_into_which_each_of_these_original_edges_is_subdivided:
+   3 3 3 3 3
+   Original_edgeid_-_numbers_in_pairs_for_subdivision_edges_(tileno_edgeno):
+   0 1 0 2 3 3 0
+   1 11 0 4 1 1 1
+   2 19 0 18 1 11 1
+   3 17 0 20 3 19 1
+   4 3 4 10 3 17 1
+
